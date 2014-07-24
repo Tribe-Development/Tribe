af553516358796c1eea23036cc780428cfb57afa
Charlie Martell <charliecmartell@gmail.com>
2014-07-24 13:49:20 -0700

    need to refactor nav bar

--- Tribe-App/Tribe-App/messageBox.swift
+++ Tribe-App/Tribe-App/messageBox.swift
@@ -10,7 +10,20 @@ import Foundation
 import UIKit
 
 class messageBox {
-    var imageView = UIImageView(frame: CGRectMake(0, 0, 320, 59));
     
-    
+//    var height: Int
+//    var width: Int
+//    
+//    
+//    //Message Box Setup
+//    let loginButton : UIButton = UIButton.buttonWithType(UIButtonType.Custom) as UIButton
+//    loginButton.frame = CGRectMake(0, 450, 320, 59)
+//    loginButton.backgroundColor = UIColorFromRGB(0x1A9A91)
+//    loginButton.addTarget(self, action: Selector("loginAlert"), forControlEvents: UIControlEvents.TouchUpInside)
+//    loginButton.setTitle("Login", forState: UIControlState.Normal)
+//    self.view.addSubview(loginButton)
+//    
+//    init(height: Int, width: Int) {
+//        self.height =
+//    }
 }
